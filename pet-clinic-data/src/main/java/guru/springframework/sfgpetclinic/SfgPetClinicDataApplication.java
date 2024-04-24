5f68addd6fcfb4c8a092ba9549019453c5751145
izaak <izaak@saicom.io>
2024-04-24 08:58:06 +0200

    refactoring to multimodule maven project. closes #1

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicDataApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicDataApplication.class, args);
+    }
+
+}
+
