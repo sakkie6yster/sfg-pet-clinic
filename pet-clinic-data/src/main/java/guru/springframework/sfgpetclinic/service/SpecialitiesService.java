197a57b78cc02980e1ef043239edb0862a12584f
izaak <izaak@saicom.io>
2024-05-02 16:03:16 +0200

    added Specialties Map Impl. closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.service;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialitiesService extends CrudService<Specialty, Long> {
+}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/VetServiceMap.java
@@ -5,6 +5,7 @@ import guru.springframework.sfgpetclinic.service.VetService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
+
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
