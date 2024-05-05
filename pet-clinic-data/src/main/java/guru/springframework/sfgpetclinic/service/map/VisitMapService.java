f5a46919e11cb845e3b191f9bb97e402a923b6f6
izaak <izaak@saicom.io>
2024-05-05 13:33:18 +0200

    Refactored name for VisitMapService. closes #48

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/VisitMapService.java
@@ -1,14 +1,13 @@
 package guru.springframework.sfgpetclinic.service.map;
 
 import guru.springframework.sfgpetclinic.model.Visit;
-import guru.springframework.sfgpetclinic.repository.VisitRepository;
 import guru.springframework.sfgpetclinic.service.VisitService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
-public class VistMapService extends AbstractMapService<Visit, Long> implements VisitService {
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
 
     @Override
     public Set<Visit> findAll() {
