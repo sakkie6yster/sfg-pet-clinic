3421ed08bb344c608c7483f2859f1b7ced534376
izaak <izaak@saicom.io>
2024-05-05 13:43:58 +0200

    Added active profiles for JPA service, tested active profiles

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/springdatajpa/SpecialtySDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/springdatajpa/SpecialtySDJpaService.java
@@ -10,7 +10,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Service
-@Profile("Springdatajpa")
+@Profile("springdatajpa")
 public class SpecialtySDJpaService implements SpecialityService {
 
     public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
